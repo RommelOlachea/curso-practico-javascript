105bf429acfbd19b74c9d5d7abe5a8f6f6303e37
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-02-23 14:59:12 -0700

    taller 3  mediana - CECC

--- README.md
+++ README.md
@@ -15,3 +15,10 @@
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JavaScript por HTML 
+
+## Taller #3: Promedio, mediana y moda
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript por HTML 
---
+++ mediana.js
@@ -0,0 +1,42 @@
+const lista1 = [
+    100,
+    200,
+    500,
+    400000000
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+function esPar(numerito){
+    if (numerito % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calcularMediaAritmetica(lista){
+    // let sumaLista = 0;
+    // for (let i=0;i<lista.length;i++) {
+    //     sumaLista += lista[i];
+    // }
+                                            //se le asigna 0, como valor iniciar, y el elemento del arreglo lista
+    const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento){
+        return valorAcumulado + nuevoElemento;
+    });
+
+    const promedioLista1 = sumaLista / lista.length;
+    return promedioLista1;
+}
+
+
+let mediana;
+
+if(esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1-1];
+    const elemento2 = lista1[mitadLista1];
+    const promedioElemento1y2 = calcularMediaAritmetica([elemento1,elemento2]);
+    mediana = promedioElemento1y2;
+} else {
+    mediana  = lista1[mitadLista1];
+}
+
