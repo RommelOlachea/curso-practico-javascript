fcd22d66261cd862ac7029ac5a77247e0b66fe63
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-02-24 10:52:18 -0700

    Finalizo taller 3 - CECC

--- moda.js
+++ moda.js
@@ -15,7 +15,7 @@ const lista1 = [
 //se declara un array de objetos
 const lista1Count = {};
 
-//el metodo map nos va a ayudar a recorrer nuestro arreglo
+//el metodo map nos va a ayudar a recorrer nuestro arreglo, al final regreso un arreglo de objetos, pero es por el ejemplo.
 lista1.map(
     function(elemento){
         if (lista1Count[elemento]){
@@ -26,7 +26,15 @@ lista1.map(
         }
     }
 );
+                   //pasamos el arreglo de objetos, a un arreglo de arreglos ( de esto {1: 3, 2: 5, 3: 2, 4: 1} a esto [Array(2), Array(2), Array(2), Array(2)])
+const listaArray = Object.entries(lista1Count).sort(function(a,b){
+    return a[1] - b[1];
+}
+);
 
-const listaArray = Object.entries(lista1Count);
+const moda = listaArray[listaArray.length-1][0];
+
+
+
 
 
